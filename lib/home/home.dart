eaa4278ad35cf5ce4a964ae16b824c214ca7f13c
Ondra Hlaváč <ondra@hlavac.cz>
2020-10-25 02:00:03 +0200

    Basic styling of the movie list

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,86 +1,101 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/all.dart';
+import 'package:pocfluttermovieapp/home/movie_service.dart';
+import 'movie.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+final moviesFutureProvider = FutureProvider.autoDispose<List<Movie>>((ref) async {
+  ref.maintainState = true;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final movieService = ref.read(movieServiceProvider);
+  final movies = await movieService.getMovies();
+  return movies;
+});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends ConsumerWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text('POC Movies App'),
+      ),
+      body: watch(moviesFutureProvider).when(
+        error: (e, s){
+          return Text("error");
+        },
+        loading: () => Center( child: CircularProgressIndicator()),
+        data: (movies) {
+          return RefreshIndicator(
+            onRefresh: () {
+              return context.refresh(moviesFutureProvider);
+            },
+            child: GridView.extent(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 0.7,
+              children: movies.map((movie) => _MovieBox(movie: movie)).toList(),
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MovieBox extends StatelessWidget {
+  final Movie movie;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const _MovieBox({Key key, this.movie}) : super (key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      children: [
+        Image.network(
+          movie.fullImageUrl,
+          fit: BoxFit.cover,
+            width: double.infinity,
+        ),
+        Positioned(
+            bottom:0,
+            left:0,
+            right:0,
+            child: _FrontBanner(text: movie.title)
+        ),
+      ],
+    );
+  }
+}
+
+class _FrontBanner extends StatelessWidget {
+  const _FrontBanner({
+    Key key,
+    @required this.text,
+}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          color: Colors.grey.shade200.withOpacity(0.5),
+          height: 60,
+          child: Center(
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/home/movie_service.dart
+++ lib/home/movie_service.dart
@@ -1,3 +1,29 @@
-class MovieService {
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../environment_config.dart';
+import 'movie.dart';
 
+final movieServiceProvider = Provider<MovieService>((ref) {
+  final config = ref.read(environmentConfigProvider);
+  return MovieService(config, Dio());
+});
+
+class MovieService {
+  MovieService(this._environmentConfig, this._dio);
+
+  final EnvironmentConfig _environmentConfig;
+  final Dio _dio;
+
+  Future<List<Movie>> getMovies() async {
+
+    final response = await _dio.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=${_environmentConfig.movieApiKey}&language=en-US&page=1",
+    );
+
+    final results = List<Map<String, dynamic>>.from(response.data['results']);
+    
+    List<Movie> movies = results.map((movieData) => Movie.fromMap(movieData)).toList(growable: false);
+
+    return movies;
+  }
 }
